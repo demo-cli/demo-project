660a5fcbb6acc9c71a56dc6a5639474427c212d8
smy <2864357164.com>
2022-10-11 15:44:39 +0800

    添加ask文件

---
+++ ask.js
@@ -0,0 +1,31 @@
+/*
+import messages from './../../../workProject/project-32/zovan-doctor/src/i18n/lang/index';
+ * @Author: smy
+ * @Date: 2022-10-11 15:39:45
+ * @LastEditors: smy
+ * @LastEditTime: 2022-10-11 15:44:02
+ * @Description: 请填写简介
+ */
+module.exports = [
+    {
+        type:'confirm',
+        name:'private',
+        message:'this resgistery is private?'
+    },
+    {
+        type:'input',
+        name:'author',
+        message:'author'
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'description'
+    },
+    {
+        type:'input',
+        name:'license',
+        message:'license'
+    }
+    
+]
